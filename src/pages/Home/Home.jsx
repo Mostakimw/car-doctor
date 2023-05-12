07433d3595deb032f69895d131fe45692c1479e6
Mostakimw <scientist.expert@gmail.com>
2023-05-12 14:05:06 +0600

    basic routes, footer, navbar, banner added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "./Banner";
+
+const Home = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-8">
+      <>
+        <Banner />
+      </>
+    </div>
+  );
+};
+
+export default Home;
